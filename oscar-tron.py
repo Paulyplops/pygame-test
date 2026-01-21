205e633f0f3d2dd6c4c601d9443c0017c4481d21
Paul Tate <Paulyplops@gmail.com>
2026-01-21 20:27:48 +0000

    Trying again on joystick.

--- oscar-tron.py
+++ oscar-tron.py
@@ -254,15 +254,15 @@ class ScoreScreen():
         if event.type == pygame.JOYAXISMOTION:
             p = event.instance_id
             player = self.players[p]
+            if player.joystick.get_axis(0) < -DEADZONE and abs( player.joystick.get_axis(1) ) < DEADZONE:
+                self.shift[p] = -1
+            if player.joystick.get_axis(0) > DEADZONE and abs( player.joystick.get_axis(1) ) < DEADZONE:
+                self.shift[p] = +1
             if self.columns[p] != 3:
-                if player.joystick.get_axis(0) < -DEADZONE and abs( player.joystick.get_axis(1) ) < DEADZONE:
-                    self.shift[p] = -1
-                if player.joystick.get_axis(0) > DEADZONE and abs( player.joystick.get_axis(1) ) < DEADZONE:
-                    self.shift[p] = +1
-            if player.joystick.get_axis(1) < -DEADZONE and abs( player.joystick.get_axis(0) ) < DEADZONE:
-                self.wheel_vels[p] = +800
-            if player.joystick.get_axis(1) > DEADZONE and abs( player.joystick.get_axis(0) ) < DEADZONE:
-                self.wheel_vels[p] = -800
+                if player.joystick.get_axis(1) < -DEADZONE and abs( player.joystick.get_axis(0) ) < DEADZONE:
+                    self.wheel_vels[p] = +800
+                if player.joystick.get_axis(1) > DEADZONE and abs( player.joystick.get_axis(0) ) < DEADZONE:
+                    self.wheel_vels[p] = -800
             if abs(player.joystick.get_axis(0) ) < DEADZONE and abs( player.joystick.get_axis(1) ) < DEADZONE and self.shift[p] != 0:
                 self.columns[p] = max( min( self.columns[p] + self.shift[p], 3 ), 0 )
 
